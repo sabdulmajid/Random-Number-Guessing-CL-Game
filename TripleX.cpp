85a7339d790b82a13d5d5861297cb2c723fe8447
Shaikh Ayman Abdul-Majid <85283195+sabdulmajid@users.noreply.github.com>
2021-06-16 21:09:46 +0300

    Initial commit

---
+++ TripleX.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Greetings Secret Agent! You are tasked with a mission to break into a complex system";
+    std::cout << std::endl;
+    std::cout << "You MUST always type in the correct code, or else the lock explodes and you die! Enter the correct code: ";
+
+    int a = 4;
+    int b = 5;
+    int c = 6;
+    int sum = a + b + c;
+    int product = a * b * c;
+    std::cout << std::endl;
+    std::cout << sum;
+    std::cout << std::endl;
+    std::cout << product;
+
+
+    return 0;
+}

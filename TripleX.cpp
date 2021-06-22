1cfadb36e161ad5168066c29691ed9ffd42879c6
sabdulmajid <ayman.hasib@outlook.com>
2021-06-22 14:37:39 +0300

    Addition to the game

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,19 +3,16 @@
 int main()
 {
     std::cout<< std::endl;
-    std::cout << "Greetings Secret Agent! You are tasked with a mission to break into a complex system";
-    std::cout << std::endl;
-    std::cout << "You MUST always type in the correct code, or else the lock explodes and you die! Enter the correct code: ";
+    std::cout << "Greetings Secret Agent! You are tasked with a mission to break into a complex system"<< std::endl;
+    std::cout << "You MUST always type in the correct code, or else the lock explodes and you die! Enter the correct code: "<< std::endl;
 
     int a = 4;
     int b = 5;
     int c = 6;
     int sum = a + b + c;
     int product = a * b * c;
-    std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
-
-
+    
     return 0;
 }

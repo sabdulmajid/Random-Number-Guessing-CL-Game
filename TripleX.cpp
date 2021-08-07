b9040ff5f1823e21b16a0913860f90ed0174ef99
sabdulmajid <ayman.hasib@outlook.com>
2021-08-07 20:56:36 +0300

    New changes to game mechanism

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
-// main() function used to 'run' code
-int main()
+void PrintIntroduction()
 {
-    std::cout<< std::endl;
-    std::cout << "Greetings Secret Agent! You are tasked with a mission to break into a complex system"<< std::endl;
-    std::cout << "You MUST always type in the correct code, or else the lock explodes and you die! Enter the correct code: "<< std::endl;
+    // Introducing the game to the players
+    std::cout << "\nGreetings Secret Agent! You are tasked with a mission to break into a complex system\n";
+    std::cout << "You MUST always type in the correct code, or else the lock explodes and you die! Enter the correct code: \n\n";
+}
 
+void PlayGame()
+{
+    PrintIntroduction();
     const int CodeA = 2;
     const int CodeB = 3;
     const int CodeC = 4;
@@ -14,29 +17,42 @@ int main()
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
+    // Outputting instructions to the terminal
     std::cout << std::endl;
-    std::cout << "- There are 3 numbers in the code." << std::endl;
-    std::cout << "- The codes add upto: " << CodeSum << std::endl;
-    std::cout << "- The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "- There are 3 numbers in the code.\n";
+    std::cout << "\n- The codes add upto: " << CodeSum;
+    std::cout << "\n- The codes multiply to give: " << CodeProduct << std::endl;
     
+    // Inputting player guesses
     int GuessA, GuessB, GuessC;
-
-    std::cout << std::endl;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
     
+
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // If statement to check if guesses are correct.
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You guessed correct! Well done.";
+        std::cout << "\nYou guessed correct! Well done.";
     }
     else
     {
-        std::cout << "You guessed incorrect. An explosion occurs. You die.";
+        std::cout << "\nYou guessed incorrect. An explosion occurs. You die.";
+    }
+}
+
+
+// main() function used to 'run' code
+int main()
+{
+    while(true)
+    {
+        PlayGame();
+        std::cin.clear(); // Used to clear any errors 
+        std::cin.ignore(); // Used to ignore any wrong input
     }
 
+    
     return 0;
 }
